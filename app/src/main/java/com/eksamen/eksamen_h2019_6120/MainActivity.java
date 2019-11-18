f66a99d50ef0dd73e8fc48cfca4b46dd683e1780
Simon <simon.wernersson@live.no>
2019-11-18 20:25:41 +0100

    Initial commit

---
+++ app/src/main/java/com/eksamen/eksamen_h2019_6120/MainActivity.java
@@ -0,0 +1,14 @@
+package com.eksamen.eksamen_h2019_6120;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
