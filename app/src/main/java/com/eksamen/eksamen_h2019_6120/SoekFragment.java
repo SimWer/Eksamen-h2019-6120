83ba8b1d9e848dd98b33365ed03d7d9fac505f61
Simon <simon.wernersson@live.no>
2019-11-20 00:35:50 +0100

    add datepicker

--- app/src/main/java/com/eksamen/eksamen_h2019_6120/SoekFragment.java
+++ app/src/main/java/com/eksamen/eksamen_h2019_6120/SoekFragment.java
@@ -1,6 +1,9 @@
 package com.eksamen.eksamen_h2019_6120;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,8 +14,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 /**
@@ -27,9 +33,10 @@ public class SoekFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private String url;
-    private EditText sokFelt;
+    private EditText sokFelt, datoFelt;
     private Button sokKnapp;
     private Context context;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     public SoekFragment() {
         // Required empty public constructor
@@ -45,14 +52,45 @@ public class SoekFragment extends Fragment {
         final View rotView = inflater.inflate(R.layout.fragment_soek_fragment, container, false);
 
         sokFelt = rotView.findViewById(R.id.sokFelt);
+        datoFelt = rotView.findViewById(R.id.datoFelt);
         sokKnapp = rotView.findViewById(R.id.sokKnapp);
         recyclerView = rotView.findViewById(R.id.recyclerTilsyn);
 
         context = rotView.getContext();
+
+        datoFelt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar kalender = Calendar.getInstance();
+                int aar = kalender.get(Calendar.YEAR);
+                int maamed = kalender.get(Calendar.MONTH);
+                int dag = kalender.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(context, android.R.style.Theme_Material_Light_Dialog, dateSetListener, aar, maamed, dag);
+                dialog.show();
+
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month += 1;
+                String dato;
+                if(month < 10) {
+                    dato = "" + "0" + dayOfMonth + "0" + month +  year;
+                } else {
+                    dato = "" + "0" + dayOfMonth + month +  year;
+
+                }
+                datoFelt.setText(dato);
+            }
+        };
+
         sokKnapp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                url = sokFelt.getText().toString();
+                url = sokFelt.getText().toString() + "&dato=" + datoFelt.getText();
                 RestController.tilsynRequest(url, context, recyclerView);
             }
         });
